f929631b97e9d9486b8d992cb0009210c7d0f2bf
roman3222 <romkakms3222@gmail.com>
2024-01-10 21:37:49 +1000

    add models and admin

--- stripe_django/shop_item/admin.py
+++ stripe_django/shop_item/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from .models import Item, Tax, Discount, Order
 
-# Register your models here.
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    """
+    Панель управления товарами
+    """
+    fieldsets = (
+        (None, {'fields': ('name', 'description', 'price', 'currency')}),
+    )
+    list_display = ('id', 'name', 'description', 'price', 'currency')
+    search_fields = ('name',)
+    list_filter = ('currency', 'price')
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    """
+    Панель управления скидками
+    """
+    fieldsets = (
+        (None, {'fields': ('name', 'amount')}),
+    )
+    list_display = ('id', 'name', 'amount')
+    search_fields = ('name',)
+    list_filter = ('amount',)
+
+
+@admin.register(Tax)
+class TaxAdmin(admin.ModelAdmin):
+    """
+    Панель для управления налогами
+    """
+    fieldsets = (
+        (None, {'fields': ('name', 'rate')}),
+    )
+    list_display = ('id', 'name', 'rate')
+    list_filter = ('rate',)
+
+
+class ItemInline(admin.TabularInline):
+    model = Order.items.through
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Панель управления заказами
+    """
+    fieldsets = (
+        (None, {'fields': ('discount', 'tax')}),
+    )
+    list_display = ('id', 'get_items', 'discount', 'tax', 'created_at', 'total_price')
+    list_filter = ('created_at',)
+    inlines = [ItemInline]
+
+    def total_price(self, obj):
+        """
+        Метод для отображения общей суммы(с учетом скидки и налога) заказа впанели управления заказом
+        """
+        return obj.total_price()
+
+    total_price.short_description = 'Total Price'
+
+    def get_items(self, obj):
+        """
+        Метод для отображения имени товара в панели управления заказами
+        """
+        return ', '.join([item.name for item in obj.items.all()])
+
+    get_items.short_description = 'Items'
--- stripe_django/shop_item/models.py
+++ stripe_django/shop_item/models.py
@@ -1,24 +1,87 @@
 from django.db import models
 
+CURRENCY_CHOICES = (
+    ('USD', 'USD'),
+    ('RUB', 'RUB'),
+    ('EUR', 'EUR'),
+)
+
 
 class Item(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Имя заказа')
-    description = models.CharField(max_length=100, verbose_name='Описание заказа', null=True, blank=True)
+    """
+    Модель товара
+    """
+    name = models.CharField(max_length=50, verbose_name='Имя товара')
+    description = models.CharField(max_length=100, verbose_name='Описание товара', null=True, blank=True)
     price = models.PositiveBigIntegerField(verbose_name='Цена')
+    currency = models.CharField(verbose_name='Валюта', choices=CURRENCY_CHOICES, default='RUB', max_length=10)
 
     class Meta:
         verbose_name = 'Товар',
         verbose_name_plural = 'Список товаров',
-        ordering = ('-name',)
 
     def __str__(self):
         return self.name
 
 
+class Discount(models.Model):
+    """
+    Модель скидки
+    """
+    name = models.CharField(max_length=100)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Скидка'
+        verbose_name_plural = 'Список скидок'
+
+    def __str__(self):
+        return f"{self.name} - {self.amount}%"
+
+
+class Tax(models.Model):
+    """
+    Модель налога
+    """
+    name = models.CharField(max_length=100)
+    rate = models.DecimalField(max_digits=5, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Налог'
+        verbose_name_plural = 'Список налогов'
+
+    def __str__(self):
+        return f"{self.name} - {self.rate}%"
+
+
 class Order(models.Model):
+    """
+    Модель заказа
+    """
     items = models.ManyToManyField(Item, verbose_name='Товары', related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, null=True, blank=True)
+    tax = models.ForeignKey(Tax, on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Заказ'
         verbose_name_plural = 'Список заказов'
 
+    def total_price(self):
+        """
+        Функция для подсчета общей суммы заказа с учетом скидки и налога
+        """
+        total = sum(item.price for item in self.items.all())
+        discount_amount = 0
+        if self.discount:
+            discount_amount = (total * self.discount.amount) / 100
+
+        tax_amount = 0
+        if self.tax:
+            tax_amount = (total * self.tax.rate) / 100
+
+        sum_price = total - discount_amount + tax_amount
+        return sum_price
+
+    def __str__(self):
+        return f"Order {self.id}"
